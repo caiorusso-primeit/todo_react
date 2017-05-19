fa9690678b80df982e77aa722b1ae2dd7e825194
Caio Russo <caiorusso@gmail.com>
2017-05-19 14:51:24 -0300

    config do updateOptions

--- backend/src/api/todo/todoService.js
+++ backend/src/api/todo/todoService.js
@@ -1,6 +1,7 @@
 const Todo = require('./todo')
 
 Todo.methods(['get', 'post', 'put', 'delete'])
+//Lembrar que a opção de trazer o registro atualizado e as validações não estão ativas por padrão
 Todo.updateOptions({new: true, runValidators: true})
 
 module.exports = Todo
