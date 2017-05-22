457d6d49ce7b50199b5dfc87ce26c1e77828462a
Caio Russo <caiorusso@gmail.com>
2017-05-21 23:38:07 -0300

    Componente PageHeader, início do form e lista

--- frontend/src/template/pageHeader.jsx
+++ frontend/src/template/pageHeader.jsx
@@ -3,5 +3,5 @@ import React from 'react'
 export default props => (
     <header className='page-header'>
         <h2>{props.name} <small>{props.small}</small></h2>
-        </header>
+    </header>
 )
